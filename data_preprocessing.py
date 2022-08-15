e20e30f7f4af56fa349cc5f5ab21e5bba29a9dca
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-08-16 01:04:30 +0600

    Splitting the dataset into the Training set and Test set

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,27 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
-
 dataset=pd.read_csv('Data.csv')
-# print(dataset)
 X=dataset.iloc[:,:-1].values
 y=dataset.iloc[:,-1].values
-
 from sklearn.impute import SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-
 imputer.fit(X[:, 1:3])
-
 X[:,1:3]=imputer.transform(X[:,1:3])
-
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 ct=ColumnTransformer(transformers=[('encode', OneHotEncoder(),[0])], remainder='passthrough')
-
 X=np.array(ct.fit_transform(X))
-
-print(X)
 
 from sklearn.preprocessing import LabelEncoder
 le=LabelEncoder()
@@ -29,3 +19,11 @@ y=le.fit_transform(y)
 
 print(y)
 
+# from sklearn.model_selection import train_test_split
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, ramdom_state = 1)
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
+
+print(y_test)
