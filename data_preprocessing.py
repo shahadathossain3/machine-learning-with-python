1f0a1455b5b6477478dd3b37b97181cada7bfbec
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-08-16 01:02:43 +0600

    Encoding Independent and dependent variable

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -15,4 +15,17 @@ imputer.fit(X[:, 1:3])
 
 X[:,1:3]=imputer.transform(X[:,1:3])
 
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct=ColumnTransformer(transformers=[('encode', OneHotEncoder(),[0])], remainder='passthrough')
+
+X=np.array(ct.fit_transform(X))
+
 print(X)
+
+from sklearn.preprocessing import LabelEncoder
+le=LabelEncoder()
+y=le.fit_transform(y)
+
+print(y)
+
