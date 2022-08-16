0326ca3008cdc8f758cf820a6420b6a07246008a
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-08-16 20:49:21 +0600

    visualising the Decision Tree Regression results (higher resolution)

--- decision_tree_regression.py
+++ decision_tree_regression.py
@@ -14,3 +14,13 @@ print(regressor)
 
 predict_new_result=regressor.predict([[6.5]])
 print(predict_new_result)
+
+X_grid=np.arange(min(X), max(X), 0.01)
+X_grid=X_grid.reshape(len(X_grid), 1)
+plt.scatter(X, y, color='red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
