3c9133ae4a08170f85d3f78927045d913d5309bf
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-08-16 21:46:07 +0600

    Visualising the Random Forest Regression  results from dataset correct (higher resolution)

--- random_forest_regression.py
+++ random_forest_regression.py
@@ -1,26 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
+regressor.fit(X, y)
 
-dataset=pd.read_csv('Position_Salaries.csv')
-X=dataset.iloc[:,1:-1].values
-y=dataset.iloc[:,-1].values
+regressor.predict([[6.5]])
 
-from sklearn.tree import DecisionTreeRegressor
-regressor=DecisionTreeRegressor(random_state=0)
-regressor_result=regressor.fit(X, y)
-
-print(regressor)
-
-predict_new_result=regressor.predict([[6.5]])
-print(predict_new_result)
-
-X_grid=np.arange(min(X), max(X), 0.01)
-X_grid=X_grid.reshape(len(X_grid), 1)
-plt.scatter(X, y, color='red')
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.title('Truth or Bluff (Random Forest Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
-
