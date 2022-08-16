fd8903f17ec1652b0f43df34e8242137d596619d
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-08-16 20:31:11 +0600

    import dataset print X and y value and y reshape , and also feature scaling

---
+++ support_vector_regression.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset=pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:,1:-1].values
+y = dataset.iloc[:,-1].values
+
+print(X)
+
+print(y)
+
+y=y.reshape(len(y),1)
+
+print(y)
+
+from sklearn.preprocessing import StandardScaler
+sc_X=StandardScaler()
+sc_y=StandardScaler()
+X=sc_X.fit_transform(X)
+y=sc_y.fit_transform(y)
+
+print(X)
+print(y)
