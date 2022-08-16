a0e6f3969aa2dd41b8aabc0042cbefcb1f2aed59
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-08-16 20:19:34 +0600

    predicting the test set results

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -23,7 +23,14 @@ X=np.array(ct.fit_transform(X))
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test=train_test_split(X,y, test_size=0.2,random_state=0)
 
+# print(y_train)
+
 
 from sklearn.linear_model import LinearRegression
 regressor=LinearRegression()
 regressor.fit(X_train,y_train)
+
+
+y_pred=regressor.predict(X_test)
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred),1),y_test.reshape(len(y_test),1)),1))
