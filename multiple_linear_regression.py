6d39f221bfe7f9d0fb041829ff4c14701c8791c3
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-08-16 20:17:52 +0600

    training the multiple linear regression model on the training set

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -7,9 +7,9 @@ dataset=pd.read_csv('50_Startups.csv')
 X=dataset.iloc[:,:-1].values
 y=dataset.iloc[:,-1].values
 
-print(X)
+# print(X)
 
-print(y)
+# print(y)
 
 
 from sklearn.compose import ColumnTransformer
@@ -17,4 +17,13 @@ from sklearn.preprocessing import OneHotEncoder
 ct=ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[3])],remainder='passthrough')
 X=np.array(ct.fit_transform(X))
 
-print(X)
+# print(X)
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test=train_test_split(X,y, test_size=0.2,random_state=0)
+
+
+from sklearn.linear_model import LinearRegression
+regressor=LinearRegression()
+regressor.fit(X_train,y_train)
