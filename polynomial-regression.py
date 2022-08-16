fe621d59b774310bb59240fcc088aa8e54323a1c
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-08-16 20:25:37 +0600

    Visualising the Linear Regression results,and Visualising the Polynomial Regression results, and also Visualising the Polynomial Regression results (for higher resolution and smoother curve)

--- polynomial-regression.py
+++ polynomial-regression.py
@@ -13,7 +13,31 @@ lin_reg.fit(X,y)
 
 
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg=PolynomialFeatures(degree=2)
+poly_reg=PolynomialFeatures(degree=4)
 X_poly=poly_reg.fit_transform(X)
 lin_reg_2=LinearRegression()
 lin_reg_2.fit(X_poly,y)
+
+plt.scatter(X, y, color='red')
+plt.plot(X, lin_reg.predict(X), color='blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+
+plt.scatter(X, y, color='red')
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color='blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+X_grid=np.arange(min(X), max(X), 0.1)
+X_grid=X_grid.reshape((len(X_grid),1))
+plt.scatter(X,y, color='red')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color='blue')
+plt.title('Truth or Bluff(Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
