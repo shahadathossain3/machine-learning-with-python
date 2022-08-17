4368146cd10071231b30824bb63a260182f9ac28
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-08-17 22:30:54 +0600

    training the logistic regression model on the training set

--- logistic_regression.py
+++ logistic_regression.py
@@ -23,3 +23,8 @@ X_train=sc.fit_transform(X_train)
 X_test=sc.transform(X_test)
 print(X_train)
 print(X_test)
+
+
+from sklearn.linear_model import LogisticRegression
+classifier=LogisticRegression(random_state=0)
+res=classifier.fit(X_train,y_train)
