c329a8c779997fba564aa32e2d6de7c3c3f4bcb8
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-08-17 22:24:53 +0600

    import library, dataset, splitting training dataset ,testing dataset, feature scaling and print X_train, y_train, X_test, y_test result

---
+++ logistic_regression.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+dataset=pd.read_csv('Social_Network_Ads.csv')
+X=dataset.iloc[:,:-1].values
+y=dataset.iloc[:,-1].values
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test= train_test_split(X,y, test_size=0.25, random_state=0)
+
+print(X_train)
+print(y_train)
+print(X_test)
+print(y_test)
+
+
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+X_train=sc.fit_transform(X_train)
+X_test=sc.transform(X_test)
+print(X_train)
+print(X_test)
