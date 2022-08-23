241eb0a9d8574ab3aaec02c43a8758b5f5ad09c3
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-08-23 23:27:21 +0600

    feature scaling

--- support_vector_machine.py
+++ support_vector_machine.py
@@ -12,3 +12,10 @@ print(X_train)
 print(y_train)
 print(X_test)
 print(y_test)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+print(X_train)
+print(X_test)
