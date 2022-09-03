473f546a51008aec1ac6027d589e204bbea86290
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-09-03 17:11:28 +0600

    Predicting a new result

--- decision_tree_classification.py
+++ decision_tree_classification.py
@@ -23,3 +23,5 @@ print(X_test)
 from sklearn.tree import DecisionTreeClassifier
 classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
 classifier.fit(X_train, y_train)
+
+print(classifier.predict(sc.transform([[30,87000]])))
