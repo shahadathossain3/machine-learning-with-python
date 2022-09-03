46ef68116cd988fe511b723fa83d0876f1a57334
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-09-03 17:07:49 +0600

    Importing the dataset,Splitting the dataset into the Training set and Test set

---
+++ decision_tree_classification.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+print(X_train)
+print(y_train)
+print(X_test)
+print(y_test)
