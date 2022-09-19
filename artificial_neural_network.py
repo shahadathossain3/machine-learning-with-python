556f93049f57cca53d74612abbfa23b54ccab0fa
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-09-20 00:09:46 +0600

    Importing the dataset

--- artificial_neural_network.py
+++ artificial_neural_network.py
@@ -2,3 +2,9 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 tf.__version__
+
+dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3:-1].values
+y = dataset.iloc[:, -1].values
+print(X)
+print(y)
