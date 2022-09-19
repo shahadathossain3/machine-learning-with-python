01a0d5819baef92db91791df5980f1ce39d86162
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-09-20 00:12:38 +0600

    Splitting the dataset into the Training set and Test set

--- artificial_neural_network.py
+++ artificial_neural_network.py
@@ -19,3 +19,6 @@ from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
 X = np.array(ct.fit_transform(X))
 print(X)
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
