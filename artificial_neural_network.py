b24d93ab96dfd003aff4d645f4c9c28ef37fe3ce
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-09-20 00:28:57 +0600

    Making the Confusion Matrix

--- artificial_neural_network.py
+++ artificial_neural_network.py
@@ -70,3 +70,8 @@ y_pred = ann.predict(X_test)
 y_pred = (y_pred > 0.5)
 print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
 
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+result=accuracy_score(y_test, y_pred)
+print(result)
