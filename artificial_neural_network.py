cef4bb2d1ae798b5bf2a92657c4794fdc9dfd5d4
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-09-20 00:13:47 +0600

    Feature Scaling

--- artificial_neural_network.py
+++ artificial_neural_network.py
@@ -22,3 +22,9 @@ print(X)
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
