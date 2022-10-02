ab140f724535d2eed24651f5d14631f4640ef7e4
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-10-02 15:29:02 +0600

    Step 3 - Flattening

--- convolutional_neural_network.py
+++ convolutional_neural_network.py
@@ -25,3 +25,5 @@ cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
 cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
 cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
 
+cnn.add(tf.keras.layers.Flatten())
+
