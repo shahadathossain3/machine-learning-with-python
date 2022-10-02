c4ee51be3ad55e58953e333a8d97153f8af9f549
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-10-02 15:24:30 +0600

    Part 1 - Data Preprocessing and Preprocessing the Training set

--- convolutional_neural_network.py
+++ convolutional_neural_network.py
@@ -1,3 +1,12 @@
 import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
 tf.__version__
+
+train_datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range = 0.2,
+                                   zoom_range = 0.2,
+                                   horizontal_flip = True)
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size = (64, 64),
+                                                 batch_size = 32,
+                                                 class_mode = 'binary')
