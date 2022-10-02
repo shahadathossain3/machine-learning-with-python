8f2c52e367879e6ac721a680db3e381da4987d2e
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-10-02 15:28:14 +0600

    Adding a second convolutional layer

--- convolutional_neural_network.py
+++ convolutional_neural_network.py
@@ -21,3 +21,7 @@ cnn = tf.keras.models.Sequential()
 
 cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=[64, 64, 3]))
 cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
+cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+
