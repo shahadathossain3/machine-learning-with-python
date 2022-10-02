1f49b10505896aa15d7b65d91bf6d5a51ad3b3ca
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-10-02 15:27:04 +0600

    Step 1 - Convolution

--- convolutional_neural_network.py
+++ convolutional_neural_network.py
@@ -18,3 +18,5 @@ test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             class_mode = 'binary')
 
 cnn = tf.keras.models.Sequential()
+
+cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=[64, 64, 3]))
