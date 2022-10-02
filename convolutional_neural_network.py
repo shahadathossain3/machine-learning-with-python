6e17d06b93c60c635167c89a640474b6deb1f24a
Shahadat Hossain <shahadathossaincode@gmail.com>
2022-10-02 15:25:20 +0600

    Preprocessing the Test set

--- convolutional_neural_network.py
+++ convolutional_neural_network.py
@@ -10,3 +10,9 @@ training_set = train_datagen.flow_from_directory('dataset/training_set',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
                                                  class_mode = 'binary')
+
+test_datagen = ImageDataGenerator(rescale = 1./255)
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size = (64, 64),
+                                            batch_size = 32,
+                                            class_mode = 'binary')
